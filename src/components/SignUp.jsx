732d591a8eac0f8f546e3094f7e6f98458067526
Angel Emanuel Villalonga <88913610+Ema-villalonga@users.noreply.github.com>
2023-11-21 12:37:51 -0300

    Created the initial project

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,20 @@
+import '../components/SignUp.css';
+
+export default function SignUp(){
+    return (
+        <section className="container-form">
+            <h1>Sign Up</h1>
+
+            <form className="form">
+                <input type="text" placeholder="Enter your name" />
+                <input type="text" placeholder="Enter your last name" />
+                <input type="email" placeholder="Enter your email address" />
+                <input type="tel" placeholder="Enter your phone number" />
+                <input type="password" placeholder="Enter your password" />
+                <input type="password" placeholder="Repeat your password" />
+
+                <button className="btn-join">Join</button>
+            </form>
+        </section>
+    )
+}
