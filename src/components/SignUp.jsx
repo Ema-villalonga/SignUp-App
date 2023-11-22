f25e6b3a679d1a15e7423f7294b323086f1a3c74
Angel Emanuel Villalonga <88913610+Ema-villalonga@users.noreply.github.com>
2023-11-22 12:03:44 -0300

    The completed project

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -6,12 +6,13 @@ export default function SignUp(){
             <h1>Sign Up</h1>
 
             <form className="form">
-                <input type="text" placeholder="Enter your name" />
-                <input type="text" placeholder="Enter your last name" />
-                <input type="email" placeholder="Enter your email address" />
-                <input type="tel" placeholder="Enter your phone number" />
-                <input type="password" placeholder="Enter your password" />
-                <input type="password" placeholder="Repeat your password" />
+                    <input type="text" placeholder="Enter your name" />
+                    <input type="text" placeholder="Enter your last name" />
+                    <input type="email" placeholder="Enter your email address" />
+                    <input type="tel" placeholder="Enter your phone number" />
+                    <input type="password" placeholder="Enter your password" />
+                    <input type="password" placeholder="Repeat your password" />
+
 
                 <button className="btn-join">Join</button>
             </form>
